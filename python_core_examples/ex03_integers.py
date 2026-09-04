cbab61d86941a663af2719d63ea5ef68afbf8fcf
pymisc <vs95054@gmail.com>
2026-09-03 17:48:42 -0700

    Added some more examples of integer arithmetic

--- python_core_examples/ex03_integers.py
+++ python_core_examples/ex03_integers.py
@@ -34,4 +34,27 @@ print("Total memory (exponent method of calculation):", total_memory_alt1)
 total_memory_alt2=(psutil.virtual_memory().total//(1024**3))
 print("Total memory (floor method of calculation):", total_memory_alt2)
 
+# Some other arithmetic with integers:
+double_cpu_count = cpu_count * 2
+half_cpu_count = cpu_count / 2
+floor_half_cpu_count = cpu_count // 2
+remainder_cpu = cpu_count % 2
 
+print("Double CPU:", double_cpu_count)
+print("Half CPUs:", half_cpu_count)
+print("Floor half CPUs:", floor_half_cpu_count)
+print("Remainder when divided by 2:", remainder_cpu)
+
+# One basic minus example
+desired_replicas = 10
+actual_replicas = 9
+missing_replicas = desired_replicas - actual_replicas
+print("Missing replicas count (if any):", missing_replicas)
+
+### just some more examples :D
+restart_count = "12"
+print(type(restart_count))
+
+restart_count = int(restart_count)
+print(type(restart_count))
+print(restart_count + 1)
