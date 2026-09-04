12886d7c228251d83ef76a972fd035f75b84afaa
pymisc <vs95054@gmail.com>
2026-09-03 17:37:26 -0700

    Added exponent and floor calculation

--- python_core_examples/ex03_integers.py
+++ python_core_examples/ex03_integers.py
@@ -1,6 +1,16 @@
 import os
 import psutil
 
+import sys
+# print(sys.stdlib_module_names)
+# print(str(sys.stdlib_module_names))
+print("\n\nTesting boolean results:")
+print("os:", "os" in sys.stdlib_module_names)
+print("subprocess:", "subprocess" in sys.stdlib_module_names)
+print("psutil:", "psutil" in sys.stdlib_module_names)
+
+
+
 cpu_count=os.cpu_count()
 print(cpu_count)
 
@@ -17,13 +27,11 @@ print(f"Available memory: {available_memory:.1f} GB")
 print(f"Percent memory utilization: {psutil.virtual_memory().percent}%")
 
 
-import sys
-# print(sys.stdlib_module_names)
-# print(str(sys.stdlib_module_names))
+# Alternate ways to check size (other integer operations)
+total_memory_alt1=(psutil.virtual_memory().total/(1024**3))
+print("Total memory (exponent method of calculation):", total_memory_alt1)
 
-print("\n\nTesting boolean results:")
-print("os:", "os" in sys.stdlib_module_names)
-print("subprocess:", "subprocess" in sys.stdlib_module_names)
-print("psutil:", "psutil" in sys.stdlib_module_names)
+total_memory_alt2=(psutil.virtual_memory().total//(1024**3))
+print("Total memory (floor method of calculation):", total_memory_alt2)
 
 
